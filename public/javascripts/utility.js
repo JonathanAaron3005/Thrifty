28810da448e4ed205855948a7af189ce5b43315c
Tix <tix.personal@gmail.com>
2023-06-18 14:56:11 +0700

    edited view.ejs

--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -3,3 +3,24 @@ const toIDR = new Intl.NumberFormat("de-DE")
 function formatInteger(n) {
     return "Rp " + toIDR.format(n) + ",00"
 }
+
+document.addEventListener("DOMContentLoaded", function(ev) {
+    document.querySelectorAll('.price-label').forEach(element => {
+        element.textContent = formatInteger(element.textContent)
+    })
+
+    function updateFontSize() {
+        const card = document.querySelector('.card');
+        const cardTitles = document.querySelectorAll('#dynamic-card-title-size');
+
+        const cardHeight = card.offsetHeight;
+        const fontSize = Math.floor(cardHeight * 0.1);
+
+        cardTitles.forEach((title) => {
+            title.style.fontSize = `${fontSize}px`;
+        });
+    }
+
+    window.addEventListener('resize', updateFontSize);
+    updateFontSize();
+})
